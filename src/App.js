2130e9b8cfad4ea2f9058196b5917868a077e6b4
Erick Bernardo <erickvictor@gmail.com>
2021-01-25 21:06:58 -0400

    Add react-router-dom and Create page Home

--- src/App.js
+++ src/App.js
@@ -1,49 +1,15 @@
-// import logo from './logo.svg'
-import { useEffect, useState } from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import './App.css'
-import Post from './components/Post'
-
-const mockPosts = [
-  {
-    id: 1,
-    likes: 20,
-    description: 'This is a post',
-    image: {
-      url: '/uploads/Squash_6d1ec8389b.JPG'
-    }
-  },
-  {
-    id: 2,
-    likes: 33,
-    description: 'The second post',
-    image: {
-      url: '/uploads/Squash_6d1ec8389b.JPG'
-    }
-  }
-]
+import Home from './pages/Home'
 
 function App() {
-  const [posts, setPosts] = useState([])
-
-  useEffect(() => {
-    const getPosts = async () => {
-      const response = await fetch('http://localhost:1337/posts')
-      const data = await response.json()
-      setPosts(data)
-    }
-    getPosts()
-  }, [])
-
   return (
     <div className='App'>
-      {posts.map((post) => (
-        <Post
-          key={post.id}
-          likes={post.likes}
-          description={post.description}
-          url={post.image && post.image.url}
-        />
-      ))}
+      <BrowserRouter>
+        <Switch>
+          <Route path='/' exact component={Home} />
+        </Switch>
+      </BrowserRouter>
     </div>
   )
 }
