d7584cb7fa3fba30a83f4dfe7ab6da68ab9bcc71
Erick Bernardo <erickvictor@gmail.com>
2021-01-25 20:56:58 -0400

    Create component Post

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg'
+import { useEffect, useState } from 'react'
+import './App.css'
+import Post from './components/Post'
+
+const mockPosts = [
+  {
+    id: 1,
+    likes: 20,
+    description: 'This is a post',
+    image: {
+      url: '/uploads/Squash_6d1ec8389b.JPG'
+    }
+  },
+  {
+    id: 2,
+    likes: 33,
+    description: 'The second post',
+    image: {
+      url: '/uploads/Squash_6d1ec8389b.JPG'
+    }
+  }
+]
 
 function App() {
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    const getPosts = async () => {
+      const response = await fetch('http://localhost:1337/posts')
+      const data = await response.json()
+      setPosts(data)
+    }
+    getPosts()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      {posts.map((post) => (
+        <Post
+          key={post.id}
+          likes={post.likes}
+          description={post.description}
+          url={post.image && post.image.url}
+        />
+      ))}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
